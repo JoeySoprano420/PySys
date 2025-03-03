f01d19c6125d5c0225fc61ee405ef8081c3a9878
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-03 17:39:57 -0500

    Create literals.py

---
+++ literals.py
@@ -0,0 +1,7 @@
+LITERALS = {
+    'NUMBER': r'\d+(\.\d+)?',      # Integer and floating point numbers
+    'STRING': r'"(\\.|[^"\\])*"',   # Double quoted strings
+    'CHARACTER': r"'(\\.|[^'\\])'", # Single quoted characters
+    'BOOLEAN': r'true|false',       # Boolean literals
+    'NULL': r'null',                # Null literals
+}
