6b46c076f261f498ddd2f0ae7d41948a7962ff0c
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-03 17:38:36 -0500

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,11 @@
+KEYWORDS = [
+    'if', 'else', 'for', 'while', 'break', 'continue', 'return',
+    'int', 'float', 'char', 'double', 'void', 'bool', 'true', 'false',
+    'typedef', 'struct', 'union', 'enum', 'sizeof', 'malloc', 'free',
+    'static', 'extern', 'const', 'inline', 'volatile', 'register',
+    'goto', 'switch', 'case', 'default', 'namespace', 'using', 'public', 
+    'private', 'protected', 'class', 'interface', 'abstract', 'extends',
+    'implements', 'new', 'delete', 'try', 'catch', 'throw', 'finally',
+    'private', 'protected', 'public', 'friend', 'explicit', 'template',
+    'this', 'super', 'abstract', 'synchronized', 'native', 'throws'
+]
