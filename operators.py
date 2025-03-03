b241bda8d1929d97b2a4c53a64e8534c1986ba99
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-03 17:39:16 -0500

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,11 @@
+OPERATORS = {
+    'ARITHMETIC': r'[+\-*/%]',
+    'ASSIGNMENT': r'=', 
+    'ASSIGNMENT_OPS': r'\+=|-=|\*=|/=|%=',
+    'LOGICAL': r'&&|\|\|',
+    'COMPARISON': r'==|!=|<|<=|>|>=',
+    'BITWISE': r'&|\||\^|~|<<|>>',
+    'INCREMENT_DECREMENT': r'\+\+|--',
+    'NOT': r'!',
+    'TERNARY': r'\?|\:',
+}
